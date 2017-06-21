0d1ecad8231a26c32a4348e48611995a62cc8fe3
Surat Teerapittayanon <steerapi@gmail.com>
2017-06-21 10:30:22 +0000

    add mbst_bp m_bin_conv and ss_bin_conv

--- binary/bst.py
+++ binary/bst.py
@@ -73,7 +73,7 @@ class ZBST(function.Function):
     def backward_gpu(self, x, gy):
         gx = cuda.elementwise(
             'T x, T gy', 'T gx',
-            'gx = abs(x) > 1 ? 0 : gy', 'bst_bwd')(
+            'gx = abs(x) > 1 ? 0 : gy', 'zbst_bwd')(
                 x[0], gy[0])
         return gx,
 
@@ -115,10 +115,55 @@ class MBST(function.Function):
     def backward_gpu(self, x, gy):
         gx = cuda.elementwise(
             'T x, T gy', 'T gx',
-            'gx = abs(x) > 1 ? 0 : gy', 'bst_bwd')(
+            'gx = abs(x) > 1 ? 0 : gy', 'mbst_bwd')(
                 x[0], gy[0])
         return gx,
 
+class MBST_BP(function.Function):
+    """Magnitude + Binary with Straight Thourgh estimator Unit w/ backward margin as well"""
+    def __init__(self, m):
+        self.m = m
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 1)
+        x_type, = in_types
+
+        type_check.expect(
+            x_type.dtype == numpy.float32,
+        )
+
+    def forward_cpu(self, x):
+        gt = x[0] > self.m
+        lt = x[0] < -self.m
+        x[0][gt] = 1
+        x[0][lt] = -1
+        x[0][~(gt | lt)] = 0
+        return x
+
+    def forward_gpu(self, x):
+        gt = x[0] > self.m
+        lt = x[0] < -self.m
+        x[0][gt] = 1
+        x[0][lt] = -1
+        x[0][~(gt | lt)] = 0
+        return x
+
+    def backward_cpu(self, x, gy):
+        gx = gy[0].copy()
+        zero_indices = numpy.abs(x[0]) > self.m
+        gx[zero_indices] = 0
+        return gx,
+
+    def backward_gpu(self, x, gy):
+        gx = cuda.elementwise(
+            'T x, T gy, T m', 'T gx',
+            'gx = abs(x) > m ? 0 : gy', 'mbst2_bwd')(
+                x[0], gy[0], self.m)
+        return gx,
+
+
+def mbst_bp(x, m):
+    return MBST_BP(m)(x)
 
 def mbst(x, m):
     return MBST(m)(x)

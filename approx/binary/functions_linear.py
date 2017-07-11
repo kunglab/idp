e49c4ae36a8ef723b6edc21474e928420ffe5706
Surat Teerapittayanon <steerapi@gmail.com>
2017-07-11 00:24:19 +0000

    add binary version

---
+++ approx/binary/functions_linear.py
@@ -0,0 +1,134 @@
+from chainer import function
+from chainer.utils import type_check
+from chainer import cuda
+import numpy
+
+def _kern():
+    return cuda.elementwise(
+        'T x', 'T y',
+        'y = x >= 0 ? 1 : -1',
+        'binarize')
+
+def _as_mat(x):
+    if x.ndim == 2:
+        return x
+    return x.reshape(len(x), -1)
+
+class LinearFunction(function.Function):
+    
+    def check_type_forward(self, in_types):
+        n_in = in_types.size()
+        type_check.expect(2 <= n_in, n_in <= 3)
+        x_type, w_type = in_types[:2]
+
+        type_check.expect(
+            x_type.dtype.kind == 'f',
+            w_type.dtype.kind == 'f',
+            x_type.ndim >= 2,
+            w_type.ndim == 2,
+            type_check.prod(x_type.shape[1:]) == w_type.shape[1],
+        )
+        if type_check.eval(n_in) == 3:
+            b_type = in_types[2]
+            type_check.expect(
+                b_type.dtype == x_type.dtype,
+                b_type.ndim == 1,
+                b_type.shape[0] == w_type.shape[0],
+            )
+
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
+        
+    def forward(self, inputs):
+        x = _as_mat(inputs[0])
+        W = inputs[1]
+
+        xp = cuda.get_array_module(*x)
+        W = xp.where(W>=0, 1, -1).astype(numpy.float32, copy=False)
+
+        olen, ilen = W.shape
+        if self.coeffs is None:
+            self.coeffs = numpy.ones(ilen)
+        coeffs = numpy.copy(self.coeffs)
+        coeffs = numpy.expand_dims(coeffs, 0)        
+        coeffs = numpy.broadcast_to(coeffs, W.shape)
+        M = xp.asarray(coeffs,numpy.float32).reshape(W.shape)
+        self.M = M
+        W = self.M*W
+        
+        if not type_check.same_types(*inputs):
+            raise ValueError('numpy and cupy must not be used together\n'
+                             'type(W): {0}, type(x): {1}'
+                             .format(type(W), type(x)))
+
+        y = x.dot(W.T).astype(x.dtype, copy=False)
+        if len(inputs) == 3:
+            b = inputs[2]
+            y += b
+        return y,
+
+    def backward(self, inputs, grad_outputs):
+        x = _as_mat(inputs[0])
+        W = inputs[1]
+        
+        xp = cuda.get_array_module(*x)
+        W = xp.where(W>=0, 1, -1).astype(numpy.float32, copy=False)
+
+        W = self.M * W
+        gy = grad_outputs[0]
+
+        if not type_check.same_types(*inputs):
+            raise ValueError('numpy and cupy must not be used together\n'
+                             'type(W): {0}, type(x): {1}'
+                             .format(type(W), type(x)))
+
+        gx = gy.dot(W).astype(x.dtype, copy=False).reshape(inputs[0].shape)
+        gW = gy.T.dot(x).astype(W.dtype, copy=False)
+        if len(inputs) == 3:
+            gb = gy.sum(0)
+            return gx, gW, gb
+        else:
+            return gx, gW
+
+
+def linear(x, coeffs, W, b=None):
+    """Linear function, or affine transformation.
+
+    It accepts two or three arguments: an input minibatch ``x``, a weight
+    matrix ``W``, and optionally a bias vector ``b``. It computes
+    .. math:: Y = xW^\\top + b.
+
+    Args:
+        x (:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Input variable, which is a :math:`(s_B, s_1, \
+            s_2, ..., s_n)`-shaped float array. Its first dimension
+            :math:`(s_B)` is assumed to be the *minibatch dimension*. The
+            other dimensions are treated as concatenated one dimension whose
+            size must be :math:`(s_1 * ... * s_n = N)`.
+        W (:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Weight variable of shape :math:`(M, N)`,
+            where :math:`(N = s_1 * ... * s_n)`.
+        b (:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Bias variable (optional) of shape
+            :math:`(M,)`.
+
+    Returns:
+        ~chainer.Variable: Output variable. A float array with shape
+        of :math:`(s_B, M)`.
+
+    .. seealso:: :class:`~chainer.links.Linear`
+
+    .. admonition:: Example
+
+        >>> x = np.random.uniform(0, 1, (3, 4)).astype('f')
+        >>> W = np.random.uniform(0, 1, (5, 4)).astype('f')
+        >>> b = np.random.uniform(0, 1, (5,)).astype('f')
+        >>> y = F.linear(x, W, b)
+        >>> y.shape
+        (3, 5)
+
+    """
+    if b is None:
+        return LinearFunction(coeffs)(x, W)
+    else:
+        return LinearFunction(coeffs)(x, W, b)
---
+++ approx/binary/links_depthwise_convolution_2d.py
@@ -0,0 +1,104 @@
+from __future__ import absolute_import
+
+import numpy
+
+from .functions_depthwise_convolution_2d import depthwise_convolution_2d
+from chainer import initializers
+from chainer import link
+from chainer import variable
+
+class IncompleteDepthwiseBinaryConvolution2D(link.Link):
+
+    """Two-dimensional depthwise convolutional layer.
+
+    This link wraps the :func:`~chainer.functions.depthwise_convolution_2d`
+    function and holds the filter weight and bias vector as parameters.
+
+    Args:
+        in_channels (int): Number of channels of input arrays. If ``None``,
+            parameter initialization will be deferred until the first forward
+            data pass at which time the size will be determined.
+        channel_multiplier (int): Channel multiplier number. Number of output
+            arrays equal ``in_channels * channel_multiplier``.
+        ksize (int or pair of ints): Size of filters (a.k.a. kernels).
+            ``ksize=k`` and ``ksize=(k, k)`` are equivalent.
+        stride (int or pair of ints): Stride of filter applications.
+            ``stride=s`` and ``stride=(s, s)`` are equivalent.
+        pad (int or pair of ints): Spatial padding width for input arrays.
+            ``pad=p`` and ``pad=(p, p)`` are equivalent.
+        nobias (bool): If ``True``, then this link does not use the bias term.
+        initialW (4-D array): Initial weight value. If ``None``, the default
+            initializer is used.
+            May also be a callable that takes ``numpy.ndarray`` or
+            ``cupy.ndarray`` and edits its value.
+        initial_bias (1-D array): Initial bias value. If ``None``, the bias
+            is set to 0.
+            May also be a callable that takes ``numpy.ndarray`` or
+            ``cupy.ndarray`` and edits its value.
+
+    .. seealso::
+       See :func:`chainer.functions.depthwise_convolution_2d`.
+
+    Attributes:
+        W (~chainer.Variable): Weight parameter.
+        b (~chainer.Variable): Bias parameter.
+
+    """
+
+    def __init__(self, in_channels, out_channels, ksize=None, stride=1, pad=0,
+                 nobias=False, initialW=None, initial_bias=None):
+        super(IncompleteDepthwiseBinaryConvolution2D, self).__init__()
+        if ksize is None:
+            out_channels, ksize, in_channels = in_channels, out_channels, None
+        self.ksize = ksize
+        self.stride = _pair(stride)
+        self.pad = _pair(pad)
+        self.channel_multiplier = out_channels
+        self.nobias = nobias
+
+        if initialW is None:
+            initialW = initializers.HeNormal(1. / numpy.sqrt(2))
+
+        with self.init_scope():
+            W_initializer = initializers._get_initializer(initialW)
+            self.W = variable.Parameter(W_initializer)
+
+            if nobias:
+                self.b = None
+            else:
+                if initial_bias is None:
+                    initial_bias = initializers.Constant(0)
+                bias_initializer = initializers._get_initializer(initial_bias)
+                self.b = variable.Parameter(bias_initializer)
+
+        if in_channels is not None:
+            self._initialize_params(in_channels)
+
+    def _initialize_params(self, in_channels):
+        kh, kw = _pair(self.ksize)
+        W_shape = (self.channel_multiplier, in_channels, kh, kw)
+        self.W.initialize(W_shape)
+        if self.b is not None:
+            self.b.initialize(self.channel_multiplier * in_channels)
+
+    def __call__(self, x, coeffs=None):
+        """Applies the depthwise convolution layer.
+
+        Args:
+            x (chainer.Variable or :class:`numpy.ndarray` or cupy.ndarray):
+                Input image.
+
+        Returns:
+            ~chainer.Variable: Output of the depthwise convolution.
+
+        """
+        if self.W.data is None:
+            self._initialize_params(x.shape[1])
+        return depthwise_convolution_2d(
+            x, coeffs, self.W, self.b, self.stride, self.pad)
+
+
+def _pair(x):
+    if hasattr(x, '__getitem__'):
+        return x
+    return x, x
